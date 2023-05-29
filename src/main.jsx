4186cacd5c9507a25f17310682aa8251cd64e845
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-05-29 17:28:31 +1000

    installing redux toolkit and creating store

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { store } from "./store";
+import { Provider } from "react-redux";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
