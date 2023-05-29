edf7093a188f4cfd4a2b756208006e68752aa914
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-05-29 22:26:14 +1000

    clearing cart with redux

--- src/features/cart/cartSlice.jsx
+++ src/features/cart/cartSlice.jsx
@@ -6,11 +6,18 @@ const initialState = {
   total: 0,
   isLoading: true,
 };
+
 const cartSlice = createSlice({
   name: "cart",
   initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [];
+    },
+  },
 });
 
 // console.log(cartSlice);
+export const { clearCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
