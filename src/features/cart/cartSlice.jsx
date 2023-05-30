7ff3c6adcc6728c20d37cbca3f00fdbf5ee2d4f0
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-05-30 19:12:54 +1000

    increase and decrease reducerss

--- src/features/cart/cartSlice.jsx
+++ src/features/cart/cartSlice.jsx
@@ -19,10 +19,20 @@ const cartSlice = createSlice({
       const itemId = action.payload;
       state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
     },
+    increase: (state, { payload }) => {
+      // console.log(state, { payload });
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount + 1;
+    },
+    decrease: (state, { payload }) => {
+      // console.log(state, { payload });
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount = cartItem.amount - 1;
+    },
   },
 });
 
 // console.log(cartSlice);
-export const { clearCart, removeItem } = cartSlice.actions;
+export const { clearCart, removeItem, increase, decrease } = cartSlice.actions;
 
 export default cartSlice.reducer;
